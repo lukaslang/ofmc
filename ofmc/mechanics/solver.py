Separated figure files. Fixed scaling issue with velocities generated by mechanical model.

ofmc/mechanics/solver.py
@@ -136,7 +136,7 @@ def check_cfl(v: np.array, dt: float, dx: float, CFL: float):
     """
     cmax = CFL * dx / np.max(np.abs(v))
     if cmax < dt:
-        print('CFL condition violated as dt={0 > {1}!\n'.format(dt, cmax))
+        print('CFL condition violated as dt={0} > {1}!\n'.format(dt, cmax))
         sys.exit()
 
 
@@ -243,7 +243,7 @@ def solve(mp: ModelParams, sp: SolverParams, rho_init, ca_init, x: np.array,
     # Define simulated laser ablation on cell-centred grid.
     cut = [0 if x <= 0.5 + mp.k / 2 and x >= 0.5 - mp.k / 2 else 1 for x in X]
     # Smoothen cut.
-    #cut = scipy.ndimage.filters.gaussian_filter1d(np.array(cut, dtype=float),
+    # cut = scipy.ndimage.filters.gaussian_filter1d(np.array(cut, dtype=float),
     #                                              sigma=1.5)
     cut_idx = 0
 

paper_figures_11.py
@@ -18,9 +18,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with OFMC.  If not, see <http://www.gnu.org/licenses/>.
 #
-# This script computes results for data created by the mechanical model.
-# Figures 10: set artvel=False
-# Figures 11: set artvel=True
+# This script computes results for data for a set velocity.
 import os
 import datetime
 import numpy as np
@@ -48,18 +46,16 @@ mp.k_on = 0
 mp.k_off = 0
 
 sp = solver.SolverParams()
-sp.n = 400
-sp.m = 100
-sp.T = 0.01
-sp.dt = 1e-6
+sp.n = 200
+sp.m = 200
+sp.T = 0.025
+sp.dt = 1e-5
 
 
 # Define initial values.
 def ca_init(x):
-    return stats.uniform.pdf(x, 0, 1) \
-        + (1 + math.sin(50 * x + math.cos(50 * x))) / 10
-#    return stats.uniform.pdf(x, 0, 1) * 20 \
-#        - math.sin(40 * x + math.cos(40 * x)) / 5
+    return stats.uniform.pdf(x, 0, 1) * 20 \
+        - math.sin(40 * x + math.cos(40 * x)) / 2
 
 
 def rho_init(x):
@@ -68,14 +64,19 @@ def rho_init(x):
 
 
 # Define parameters of artificial velocity field.
-c0 = 0.05 / 2
-v0 = 30
-tau0 = 0.005
+c0 = mp.k / 2
+v0 = 5
+tau0 = 0.05
 tau1 = 0.1
 
 
 def vel(t: float, x: float) -> float:
     return velocity.vel(t, x - 0.5, c0, v0, tau0, tau1)
+
+
+# Define zero velocity.
+# def vel(t: float, x: float) -> float:
+#    return 0
 
 
 # Set evaluations.
@@ -105,24 +106,26 @@ plt.close()
 x = np.array(np.linspace(0, 1, num=25))
 
 # Run solver.
-if artvel is True:
-    rho, ca, v, sigma, x, idx = solver.solve(mp, sp, rho_init, ca_init, x,
-                                             vel=vel)
-else:
-    rho, ca, v, sigma, x, idx = solver.solve(mp, sp, rho_init, ca_init, x)
+rho, ca, v, sigma, x, idx = solver.solve(mp, sp, rho_init, ca_init, x,
+                                         vel=vel)
 
+plt.figure()
+for t in np.arange(0, 200, 25):
+    plt.plot(ca[t, :])
+plt.show()
+plt.close()
 
 # Compute mean from staggered grid.
 v = (v[:, 0:-1] + v[:, 1:]) / 2
 
-# TODO: Fix scaling of velocities.
-# Scale velocities.
-m, n = v.shape
-hx, hy = 1.0 / (m - 1), 1.0 / (n - 1)
-# v = v * hy / hx
+# Scale velocities to time interval [0, 1].
+v = v * sp.T
 
 # Compute source.
 source = mp.k_on - mp.k_off * ca
+
+# Scale source to time interval [0, 1].
+source = source * sp.T
 
 # Set name and create folder.
 name = 'mechanical_model_artvel_{0}_simulated'.format(str(artvel).lower())
@@ -138,11 +141,11 @@ ph.saveimage(resfolder, '{0}-k'.format(name), source, 'k')
 ph.savevelocity(resfolder, '{0}-v'.format(name), ca, v)
 
 # Set regularisation parameter.
-alpha0 = 5e-2
+alpha0 = 1e-1
 alpha1 = 1e-3
-alpha2 = 1e-1
-alpha3 = 1e-1
-beta = 5e-3
+alpha2 = 1e-2
+alpha3 = 1e-2
+beta = 1e-3
 
 # Define concentration.
 img = ca[idx + 1:, :]
@@ -165,8 +168,6 @@ ph.savestrainrate(resfolder, name, img, vel)
 
 # Compute and output errors.
 err_v = np.abs(vel - v[idx+1:, :])
-ph.saveimage(resfolder, '{0}-error_v'.format(name),
-             err_v, 'Absolute difference in v.')
+ph.saveimage(resfolder, '{0}-error_v'.format(name), err_v)
 err_k = np.abs(k - source[idx+1:, :])
-ph.saveimage(resfolder, '{0}-error_k'.format(name),
-             err_k, 'Absolute difference in k.')
+ph.saveimage(resfolder, '{0}-error_k'.format(name), err_k)